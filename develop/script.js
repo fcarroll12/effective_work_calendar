f8069699fcd74287fce6261f1ff92bf28fdcc5ff
Fiona Carroll <fiona.carroll@me.com>
2022-04-14 13:48:48 -0700

    worked with tutor and updated/fixed my javascript

--- develop/script.js
+++ develop/script.js
@@ -2,34 +2,50 @@ var today = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(today);
 
 
-var hour = document.querySelectorAll ('.hour');
-var presentTime = moment().hour("H");
-var idHour = ".hour- ";
-console.log(presentTime);
+// var hour = document.querySelectorAll ('.hour');
+// var presentTime = moment().hour("H");
+// var idHour = ".hour- ";
+// console.log(presentTime);
+// I got help from a tutor with understanding this assignment. Hence resubmitting. 
+var currentHour = moment().hour();
+console.log(currentHour);
 
-for (var i = 0; i <= idHour; i++) {
-    console.log(idHour[i].textContent)
+$('.time-block').each(function () {
+    var timeblockHour = parseInt($(this).attr("id").split("-")[1]);
+    // console.log (currentHour);
+    if (currentHour === timeblockHour) {
+        $(this).addClass("present");
+    } else if (timeblockHour < currentHour) {
+        $(this).addClass("past");
+    } else {
+        $(this).addClass("future");
+    }
+
+});
+
+// // for (var i = 0; i <= idHour; i++) {
+// //     console.log(idHour[i].textContent)
     
-    if (presentTime === idHour) {
-        $(idHour).addClass("present");
-        idHour.children('.description').children().css('background-color', 'green');
-    }
-    else if  (presentTime < idHour) {
-        $(idHour).addClass("past");
-        idHour.children('.description').children().css('background-color', 'grey');
-    } 
-    else {
-        $(idHour).addClass("future");
-        idHour.children('.description').children().css('background-color', 'red');
-    }
-}
+// //     if (currentHour === timeblockHour) {
+// //         $(idHour).addClass("present");
+// //         idHour.children('.description').children().css('background-color', 'green');
+// //     }
+// //     else if  (presentTime < idHour) {
+// //         $(idHour).addClass("past");
+// //         idHour.children('.description').children().css('background-color', 'grey');
+// //     } 
+// //     else {
+// //         $(idHour).addClass("future");
+// //         idHour.children('.description').children().css('background-color', 'red');
+// //     }
+// // }
 
 
-//track current time in calendar 
+// //track current time in calendar 
 
-//set color to present past and future based on present time
-//past color is grey, present color is red, future color is green
-//update the color based on the present time
+// //set color to present past and future based on present time
+// //past color is grey, present color is red, future color is green
+// //update the color based on the present time
 
 
 //save event in local storage
@@ -44,18 +60,23 @@ var column = (localStorage.getItem("col"));
 description.textContent = column;
 
 saveBtn.on('click', function () {
-    var button = $(this);
-    var description = workHours;
-    description = button.prev().text();
-    var id = button.closest(description).attr('id');
-    console.log(id);
-    console.log(description); 
-    localStorage.setItem(id, description, column);
-    event.preventDefault();
+    var data = $(this).siblings('.description').val();
+    var description = $(this).parent().attr('id');
+
+    localStorage.setItem(description, data);
 })
+
+//get data from local storage and display to the HTMl
+//target the time-block  then target text area and set value equal to what you get out of local storage
+$('#hour-9 .description').val(localStorage.getItem('hour-9'));
+$('#hour-10 .description').val(localStorage.getItem('hour-10'));
+$('#hour-11 .description').val(localStorage.getItem('hour-11'));
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+$('#hour-13 .description').val(localStorage.getItem('hour-13'));
+$('#hour-14 .description').val(localStorage.getItem('hour-14'));
+$('#hour-15 .description').val(localStorage.getItem('hour-15'));
+$('#hour-16 .description').val(localStorage.getItem('hour-16'));
+$('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
 //create different keys to save to local storage
 
-//attribute to use as key: use the id and DOM Traversal to set item in local storage
-//when page is refreshed the saved event stays on page
-//prevent the page from auto default using event.preventDefault
